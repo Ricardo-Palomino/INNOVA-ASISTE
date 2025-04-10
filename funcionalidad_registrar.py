f12ca7462111cb288df5e329e43c704b0874afa7
Sebas404040 <delgadopalitogarbage@gmail.com>
2025-04-09 21:55:18 -0500

    feat: :sparkles: Function creation
    Function creation for putting all the infromation for register the student
    
    none

---
+++ funcionalidad_registrar.py
@@ -0,0 +1,42 @@
+def registrar_student():
+    import json
+    print("Ingrese el numero de documento del estudiante:")
+    documento = input("Documento: ")
+    if not documento.isdigit():
+        print("El documento debe ser un número.")
+        return
+    try:
+        with open("estudiantes.json", "r") as cargar:
+            estudiantes = json.load(cargar)
+    except (FileNotFoundError, json.JSONDecodeError):
+        estudiantes = []
+    if documento in estudiantes:
+        print("El documento ya existe.")
+        return
+    else:
+        print("Ingrese los siguientes datos del estudiante:")
+        nombre = input("Nombre: ")
+        if not nombre.isalpha():
+            print("El nombre debe contener solo letras.")
+            return
+        apellido = input("Apellido: ")
+        if not apellido.isalpha():
+            print("El apellido debe contener solo letras.")
+            return
+        grado = input("Grado: ")
+        if not grado.isdigit():
+            print("El grado debe ser un número.")
+            return
+        if int(grado) < 1 or int(grado) > 11:
+            print("El grado debe estar entre 1 y 11.")
+            return
+        estudiante= {
+            "nombre": nombre,
+            "apellido": apellido,
+            "grado": grado
+        }
+        estudiantes[documento] = estudiante
+        with open("estudiantes.json", "w") as guardar:
+            json.dump(estudiantes, guardar)
+        print("Estudiante registrado correctamente.")
+registrar_student()
